a5eddeb140b0c9bce8b040e8ab3a36f93e2021a4
Lina Beltrán <lmbd92@hotmail.com>
2021-05-01 12:31:26 -0500

    Panel text terminado y btn descargar funcional

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const panel = document.querySelector("#panel");
 const btnClose = document.querySelector("#panel-button-close");
 const btnImg = document.querySelector("#btn-img");
 const btnText = document.querySelector("#btn-text");
+const btnDescargar = document.querySelector("#btn-descargar");
+const memeContainer = document.querySelector(".meme-container");
 const panelImg = document.querySelector("#panel-imagen");
 const panelText = document.querySelector("#panel-texto");
 const body = document.querySelector("body");
@@ -112,10 +114,137 @@ const reiniciar = () => {
   filtroSaturado.value = "100";
   filtroNegativo.value = "0";
 };
-
-btnRestart.addEventListener("click", () =>{
+// Botón reset panel imagen
+btnRestart.addEventListener("click", () => {
   reiniciar();
   actualizarFiltros();
 });
 
+//Panel Texto
 
+const textSup = document.querySelector("#sup-text"); //Input
+const checkSup = document.querySelector("#sup-check"); // CheckBox
+const textInf = document.querySelector("#inf-text"); //Input
+const checkInf = document.querySelector("#inf-check"); // CheckBox
+const memeTextSup = document.querySelector("#top-text"); // <p>
+const memeTextInf = document.querySelector("#bottom-text"); // <p>
+const optionFont = document.querySelector("#input-font"); // Lista tipos de letra
+const tamanioText = document.querySelector("#font-size"); // Tamaño de letra
+const btnLeft = document.querySelector("#btn-text-left"); // botón text izq
+const btnCenter = document.querySelector("#btn-text-center"); // botón text centro
+const btnRight = document.querySelector("#btn-text-right"); // botón text der
+const colorText = document.querySelector("#text-color");
+const colorFondoText = document.querySelector("#fondo-color-text");
+const sinFondoText = document.querySelector("#fondo-check");
+const btnSinContorno = document.querySelector("#strokeNone"); //bnt contorno
+const btnContornoClaro = document.querySelector("#strokeLight"); //bnt contorno
+const btnContornoOscuro = document.querySelector("#strokeDark"); //bnt contorno
+const labelColorText = document.querySelector("#spantext-color"); //Span color text
+const labelColorFondotxt = document.querySelector("#spantext-colorFondo"); // Span Color fondo text
+
+// Asignar a la etiqueta <p> superior el texto ingresado por el usuario
+textSup.addEventListener("input", () => {
+  memeTextSup.innerText = textSup.value;
+});
+
+// Asignar a la etiqueta <p> inferior el texto ingresado por el usuario
+textInf.addEventListener("input", () => {
+  memeTextInf.innerText = textInf.value;
+});
+
+// Deshabilitar el texto y textArea superior
+checkSup.addEventListener("click", () => {
+  if (checkSup.checked) {
+    memeTextSup.classList.add("ocultar");
+    textSup.disabled = true;
+  } else {
+    memeTextSup.classList.remove("ocultar");
+    textSup.disabled = false;
+  }
+});
+
+// Deshabilitar el texto y textArea inferior
+checkInf.addEventListener("click", () => {
+  if (checkInf.checked) {
+    memeTextInf.classList.add("ocultar");
+    textInf.disabled = true;
+  } else {
+    memeTextInf.classList.remove("ocultar");
+    textInf.disabled = false;
+  }
+});
+
+//Cambiar tipo de letra para texto superior e inferior
+optionFont.addEventListener("change", () => {
+  memeTextSup.style.fontFamily = optionFont.value;
+  memeTextInf.style.fontFamily = optionFont.value;
+});
+
+//Cambiar tamaño de letra para texto superior e inferior
+tamanioText.addEventListener("change", () => {
+  memeTextSup.style.fontSize = `${tamanioText.value}px`;
+  memeTextInf.style.fontSize = `${tamanioText.value}px`;
+});
+//Alinear texto a la izquierda
+btnLeft.addEventListener("click", () => {
+  memeTextSup.style.textAlign = "left";
+  memeTextInf.style.textAlign = "left";
+});
+//Alinear texto al centro
+btnCenter.addEventListener("click", () => {
+  memeTextSup.style.textAlign = "center";
+  memeTextInf.style.textAlign = "center";
+});
+//Alinear texto a la derecha
+btnRight.addEventListener("click", () => {
+  memeTextSup.style.textAlign = "right";
+  memeTextInf.style.textAlign = "right";
+});
+
+//Contornos texto superior e inferior
+btnSinContorno.addEventListener("click", () => {
+  memeTextSup.style.textShadow = "";
+  memeTextInf.style.textShadow = "";
+});
+//Contorno claro texto superior e inferior
+btnContornoClaro.addEventListener("click", () => {
+  memeTextSup.style.textShadow = `-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white`;
+  memeTextInf.style.textShadow = `-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white`;
+});
+//Contorno oscuro texto superior e inferior
+btnContornoOscuro.addEventListener("click", () => {
+  memeTextSup.style.textShadow = `-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black`;
+  memeTextInf.style.textShadow = `-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black`;
+});
+
+//Color texto
+colorText.addEventListener("input", () => {
+  memeTextSup.style.color = colorText.value;
+  memeTextInf.style.color = colorText.value;
+  labelColorText.textContent = colorText.value;
+});
+
+colorFondoText.addEventListener("input", () => {
+  memeTextInf.style.backgroundColor = colorFondoText.value;
+  memeTextSup.style.backgroundColor = colorFondoText.value;
+  labelColorFondotxt.textContent = colorFondoText.value;
+});
+
+sinFondoText.addEventListener("click", () => {
+  if (sinFondoText.checked) {
+    memeTextSup.style.backgroundColor = "transparent";
+    memeTextInf.style.backgroundColor = "transparent";
+    colorFondoText.disabled = true;
+  } else {
+    memeTextInf.style.backgroundColor = colorFondoText.value;
+    memeTextSup.style.backgroundColor = colorFondoText.value;
+    colorFondoText.disabled = false;
+  }
+});
+
+btnDescargar.addEventListener("click", () => {
+  domtoimage.toBlob(memeContainer)
+  .then(function (blob) {
+    window.saveAs(blob, "meme.png");
+  });
+});
